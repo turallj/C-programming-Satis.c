38ec3547131359633fd2432c7dc1ecc26feb5532
Tural Cavadov <tural.javadzade@icloud.com>
2026-03-11 09:35:58 +0400

    Create a user information collection program
    This program collects user information including name, age, university, and specialization, and then displays it.

---
+++ code01.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() {
+
+    char ad[30];
+    char soyad[30];
+    int yas;
+    char universitet[50];
+    char ixtisas[50];
+
+    printf("Adinizi daxil edin: ");
+    scanf("%s", ad);
+
+    printf("Soyadinizi daxil edin: ");
+    scanf("%s", soyad);
+
+    printf("Yasinizi daxil edin: ");
+    scanf("%d", &yas);
+    getchar();   // enter-i temizlemek ucun
+
+    printf("Universitetinizi daxil edin: ");
+    fgets(universitet, 50, stdin);
+
+    printf("Ixtisasinizi daxil edin: ");
+    fgets(ixtisas, 50, stdin);
+
+    printf("\n--- Daxil etdiyiniz melumatlar ---\n");
+    printf("Ad: %s\n", ad);
+    printf("Soyad: %s\n", soyad);
+    printf("Yas: %d\n", yas);
+    printf("Universitet: %s", universitet);
+    printf("Ixtisas: %s", ixtisas);
+
+    return 0;
+}
+
